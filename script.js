df45578abbaa87d89adb4b6d2bac5a00e0899a6e
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-02-13 14:11:04 -0600

    I changed the website so people ncould rate my website and I made it so people could choose a icon

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-// let apple = 1;
-// let myname = '';
-
-// function applepie(){
-//     let ingredientX = 5;
-//     console.log(apple);
-// }
-
-// let pie2 = function(cinnamon) {
-
-//     console.log(apple + cinnamon);
-
-// }
-
-// // console.log(ingredientX);
-
-// applepie();
-// pie2(9)
-
 function whodat() {
 
     let myname = prompt("What is your name?");
-    if (myname != 'Jared') {
-        alert('ERROR HACKER ERROR');
-    } else {
-    alert("Hello, " + myname);
-    }
+    console.log(myname);
+     alert("Hello, " + myname);
 }
 
 whodat();
@@ -34,24 +12,49 @@ whodat();
 
 function printInput() {
     var input = document.getElementById("inputBox").value + '<br>';
+    let x = document.getElementById("Icons").innerHTML
+    console.log(x)
     // <br> tag allow you to create a new line for your comment section
-    document.getElementById("sampleText").innerHTML  = input + document.getElementById("sampleText").innerHTML;
-    // This is how to add more the one input and print it on your website
-}
-
-
-
-
-function loveTheBees() {
-
-    let text = prompt("Do you love bees?");
-    text = text.toUpperCase();
-    if (text != 'yes'){
-        alert("You're not nice.")
-    } else {
-        alert("You're such a great person");
-    }
+    document.getElementById("sampleText").innerHTML  = document.getElementById("Icons").innerHTML + input + document.getElementById("sampleText").innerHTML;
+    // "+" is how to add more the one input and print it on your website
 
 }
 
-loveTheBees();
+
+let text = prompt("Do you love bees?")
+while (text.toLowerCase() != "yes") {
+
+    text = prompt("Do you love bees?");
+
+}
+
+
+function tryAgain(){
+    let myUsername = prompt("What is your username?");
+    let secondName = " ";
+   
+    while(myUsername != secondName){
+        if (myUsername != secondName){
+            secondName = prompt("Please re-enter your username!")
+        }
+            else{
+                alert(`Hello ${thisName}!`);
+            }
+        }
+
+        // Line 93 and 80 has back tic-marks and code (${insert code}) that allows you to use code in a sentence
+}
+tryAgain();
+
+let rating = prompt("How many star would you rate my website? From 1-5");
+ console.log(rating)
+
+
+for (let i = 0; i < rating && i < 5; i++){
+     console.log(i);
+    // Use single quotes for the inside of your code and use double quotes for the out side of the code in the paraenthesis
+    document.write("<img class = 'ratingPictures' src = 'preview.jpg'/>");
+}
+
+    
+
