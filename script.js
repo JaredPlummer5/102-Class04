2c361e209f3d20afc2491dedbc9a36901d7aa6bd
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-02-14 18:40:16 -0600

    The code was breaking every time I entered anything that wasnt one of the icon array names

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function whodat() {
+// function whodat() {
 
-    let myname = prompt("What is your name?");
-    console.log(myname);
-     alert("Hello, " + myname);
-}
+//     let myname = prompt("What is your name?");
+//     console.log(myname);
+//      alert("Hello, " + myname);
+// }
 
-whodat();
+// whodat();
+
+// The function above prompts the user to enter their name
+// Then the code takes the input and put it in the console 
+// Then an alert will pop up and say  "Hello, 'input'"
 
 
 
@@ -17,86 +21,69 @@ function printInput() {
     // "+" is how to add more the one input and print it on your website
 }
 
+let userAns = "";
+
+ function rating(){
+    let rating = prompt("How many star would you rate my website? From 1-5");
+    document.getElementById('btn').style.visibility= 'hidden';
+    for (let i = 0; i < rating && i < 5; i++){
+        document.getElementById("thisPar").innerHTML += "<img class = 'ratingPictures' src = 'preview.jpg'/>"
+    }
+}
+
+
 function printIcon() {
     let userChoice = prompt("Chose an icon by typing house, user, star, or hippo!!!!!")
+    const iconsArray = ["house", "user", "star", "hippo"]
+
+   
+
+
+    while(userChoice !== iconsArray){
+        if (userChoice == iconsArray[0]){
+            document.getElementById("houseIcon");
+
+
+            document.getElementById("userIcon").remove();
+            document.getElementById("starIcon").remove();
+            document.getElementById("hippoIcon").remove();
     
-    if (userChoice == "house"){
-        document.getElementById("houseIcon");
+    
+        }else if(userChoice == iconsArray[1]){
 
-        document.getElementById("userIcon").remove();
-        document.getElementById("starIcon").remove();
-        document.getElementById("hippoIcon").remove();
-//         // document.getElementById("userIcons2").remove();
-//         // document.getElementById("userIcons3").remove();
-//         // document.getElementById("userIcons4").remove();
-//         // document.getElementById("submitBtn1")
-//         // document.getElementById("submitBtn2").remove();
-//         // document.getElementById("submitBtn3").remove();
-//         // document.getElementById("submitBtn4").remove();
+            document.getElementById("userIcon");
+
+
+            document.getElementById("houseIcon").remove();
+            document.getElementById("starIcon").remove();
+            document.getElementById("hippoIcon").remove();
+
+    
+        }else if(userChoice == iconsArray[2]){
+
+            document.getElementById("starIcon");
+
+
+            document.getElementById("houseIcon").remove();
+            document.getElementById("userIcon").remove();
+            document.getElementById("hippoIcon").remove();
     
     
-    }else if(userChoice == "user"){
+        }else if(userChoice == iconsArray[3]){
 
-        document.getElementById("userIcon");
+            document.getElementById("hippoIcon");
 
 
-        document.getElementById("houseIcon").remove();
-        document.getElementById("starIcon").remove();
-        document.getElementById("hippoIcon").remove();
-//         // document.getElementById("userIcons2");
-//         // document.getElementById("userIcons1").remove();
-//         // document.getElementById("userIcons3").remove();
-//         // document.getElementById("userIcons4").remove();
-//         // document.getElementById("submitBtn2")
-//         // document.getElementById("submitBtn1").remove();
-//         // document.getElementById("submitBtn3").remove();
-//         // document.getElementById("submitBtn4").remove();
+            document.getElementById("houseIcon").remove();
+            document.getElementById("userIcon").remove();
+            document.getElementById("starIcon").remove();
+    
+        }else{
+            alert("ERROR")
         
+            userChoice = prompt("Chose an icon by typing house, user, star, or hippo!!!!!")
     
-    
-    }else if(userChoice == "star"){
-
-
-        document.getElementById("starIcon");
-
-
-        document.getElementById("houseIcon").remove();
-        document.getElementById("userIcon").remove();
-        document.getElementById("hippoIcon").remove();
-//         // document.getElementById("userIcons3");
-//         // document.getElementById("userIcons1").remove();
-//         // document.getElementById("userIcons2").remove();
-//         // document.getElementById("userIcons4").remove();
-//         // document.getElementById("submitBtn3")
-//         // document.getElementById("submitBtn1").remove();
-//         // document.getElementById("submitBtn2").remove();
-//         // document.getElementById("submitBtn4").remove();
-        
-    
-    
-    }else if(userChoice == "hippo"){
-
-        document.getElementById("hippoIcon");
-
-
-        document.getElementById("houseIcon").remove();
-        document.getElementById("userIcon").remove();
-        document.getElementById("starIcon").remove();
-
-
-//         // document.getElementById("userIcons4");
-//         // document.getElementById("userIcons1").remove();
-//         // document.getElementById("userIcons2").remove();
-//         // document.getElementById("userIcons3").remove();
-//         // document.getElementById("submitBtn4")
-//         // document.getElementById("submitBtn1").remove();
-//         // document.getElementById("submitBtn2").remove();
-//         // document.getElementById("submitBtn3").remove();
-        
-    
-    
-    }else{
-        alert("ERROR")
+        }
     }
 }
 printIcon();
@@ -109,22 +96,25 @@ while (text.toLowerCase() != "yes") {
 }
 
 
-function tryAgain(){
-    let myUsername = prompt("What is your username?");
-    let secondName = " ";
+// function tryAgain(){
+//     let myUsername = prompt("What is your username?");
+//     let secondName = " ";
    
-    while(myUsername != secondName){
-        if (myUsername != secondName){
-            secondName = prompt("Please re-enter your username!")
-        }
-            else{
-                alert(`Hello ${thisName}!`);
-            }
-        }
+//     while(myUsername != secondName){
+//         if (myUsername != secondName){
+//             secondName = prompt("Please re-enter your username!")
+//         }
+//             else{
+//                 alert(`Hello ${thisName}!`);
+//             }
+//         }
 
-        // Line 93 and 80 has back tic-marks and code (${insert code}) that allows you to use code in a sentence
-}
-tryAgain();
+//         // Line 93 and 80 has back tic-marks and code (${insert code}) that allows you to use code in a sentence
+// }
+// tryAgain();
+
+// The function above is working code that prompts the user to enter a user name twice. 
+//If the second input doesn't match the first the code will keep them there until the second input matches the first
 
 // let rating = prompt("How many star would you rate my website? From 1-5");
 //  console.log(rating)
@@ -135,15 +125,7 @@ tryAgain();
 //     // Use single quotes for the inside of your code and use double quotes for the out side of the code in the paraenthesis
 //     document.write("<img class = 'ratingPictures' src = 'preview.jpg'/>");
 // }
- let userAns = "";
-
- function rating(){
-    let rating = prompt("How many star would you rate my website? From 1-5");
-    document.getElementById('btn').style.visibility= 'hidden';
-    for (let i = 0; i < rating && i < 5; i++){
-        document.getElementById("thisPar").innerHTML += "<img class = 'ratingPictures' src = 'preview.jpg'/>"
-    }
-}
+ 
 
 
     
